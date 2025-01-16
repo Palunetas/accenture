2465624fabc02f53c5ff93839facfa979629d189
Gustavo Orleans <medinaornelasgustavo@gmail.com>
2025-01-16 16:38:26 -0600

    clean trash files

--- library/business/user-mcsv/target/classes/import.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO usuarios (name, password, email, enable) VALUES ('Juan Pérez', '123456', 'juan.perez@example.com', true);
-INSERT INTO usuarios (name, password, email, enable) VALUES ('Ana Gómez', 'password123', 'ana.gomez@example.com', false);
-INSERT INTO usuarios (name, password, email, enable) VALUES ('Carlos López', 'mySecret123', 'carlos.lopez@example.com', true);
-INSERT INTO usuarios (name, password, email, enable) VALUES ('Lucía Fernández', 'qwertyuiop', 'lucia.fernandez@example.com', true);
-INSERT INTO usuarios (name, password, email, enable) VALUES ('Pedro Martínez', '123qwert', 'pedro.martinez@example.com', false);
